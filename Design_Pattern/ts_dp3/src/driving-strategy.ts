4b139fa7b52d4b70f1a01437ad2260f627874a97
mykks32 <srikri5462ydv@gmail.com>
2025-12-28 14:50:07 +0545

    feat: Strategy Pattern

---
+++ Design_Pattern/ts_dp3/src/driving-strategy.ts
@@ -0,0 +1,25 @@
+import {IDrivingStrategy} from "./interfaces/IDrivingStrategy";
+
+export class EcoMode implements IDrivingStrategy {
+    public drive(): void {
+        console.log("Eco Driving Mode");
+    }
+}
+
+export class SportMode implements IDrivingStrategy {
+    public drive(): void {
+        console.log("Sports Driving Mode");
+    }
+}
+
+export class CarController {
+    private drivingStrategy: IDrivingStrategy
+
+    public setDrivingStrategy(drivingStrategy: IDrivingStrategy): void {
+        this.drivingStrategy = drivingStrategy;
+    }
+
+    public drive(): void {
+        this.drivingStrategy.drive();
+    }
+}
